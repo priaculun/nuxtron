36b05572929deeda1954651c73c3c4d2c18580ab
hh <gj.jk>
2019-05-18 11:13:59 +0700

    First Commit
    Electron + NuxtJs
    https://github.com/saltyshiomix/nuxtron

---
+++ renderer/layouts/default.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app :dark="ubahGan">
+    <v-toolbar color="primary" dark>
+      <v-toolbar-side-icon @click.native.stop="drawer = !drawer" />
+      <v-toolbar-title @click="ubahGan=!ubahGan">Go Market Place</v-toolbar-title>
+    </v-toolbar>
+    <v-navigation-drawer v-model="drawer" left fixed app>
+      <v-list>
+        <v-list-tile router nuxt :href="resolve('home')">
+          <v-list-tile-title>Home</v-list-tile-title>
+        </v-list-tile>
+        <v-list-tile router nuxt :href="resolve('about')">
+          <v-list-tile-title>About</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <main>
+      <v-content>
+        <v-container fluid>
+          <nuxt />
+        </v-container>
+      </v-content>
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      ubahGan: false
+    };
+  },
+}
+</script>
+
+<style scoped>
+  .title {
+    padding-left: 20px;
+  }
+</style>
---
+++ renderer/plugins/vuetify.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import colors from 'vuetify/es5/util/colors';
+
+// You can also specify those components you are going to use for "a la carte" build:
+// https://github.com/vuetifyjs/nuxt/blob/master/template/plugins/vuetify.js
+// https://github.com/vuetifyjs/a-la-carte/blob/master/template/src/main.js
+
+Vue.use(Vuetify, {
+  theme: {
+    // primary: '#9c27b0', // a color that is not in the material colors palette
+    // accent: colors.grey.darken3,
+    // secondary: colors.amber.darken3,
+    // info: colors.teal.lighten1,
+    // warning: colors.amber.darken2,
+    // error: colors.deepOrange.accent4,
+    // success: colors.green.accent3,
+    primary: '#0CDAEB', // a color that is not in the material colors palette
+    accent: '#119FAB',
+    secondary: '#003B54',
+    info: colors.teal.lighten1,
+    warning: colors.amber.darken2,
+    error: colors.deepOrange.accent4,
+    success: colors.green.accent3,
+  },
+});
